0dddc5e7116818bc65865a11a7e5eec085fcc1b8
Dariusz Rybicki <darrarski@gmail.com>
2016-08-05 00:39:42 +0200

    Revert "Add bash script for running tests"
    This reverts commit 2d2e7e16fda2ba28dc933d6b31274e172876e2d4.

--- tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xcodebuild test -project DRObservableArrayExample.xcodeproj -scheme 'Tests iOS' -destination 'platform=iOS Simulator,name=iPhone 5s,OS=latest' ONLY_ACTIVE_ARCH=NO | xcpretty
