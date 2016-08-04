f2bdddc23f8153d5047e740312e0c5b1aaa90e91
Dariusz Rybicki <darrarski@gmail.com>
2016-08-04 20:34:36 +0200

    Add iOS Test target and build scheme

---
+++ DRObservableArrayTests-iOS/DRObservableArrayTests_iOS.m
@@ -0,0 +1,19 @@
+//
+//  Created by Dariusz Rybicki on 04/08/16.
+//  Copyright © 2016 Darrarski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DRObservableArrayTests_iOS : XCTestCase
+
+@end
+
+@implementation DRObservableArrayTests_iOS
+
+- (void)testExample
+{
+    XCTAssert(true, @"Example test");
+}
+
+@end
