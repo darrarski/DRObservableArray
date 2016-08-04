eba0b1f6eed459f9eb1d2d83c9d4b182d2602f09
Dariusz Rybicki <darrarski@gmail.com>
2016-08-04 18:14:24 +0200

    Refactor DRgenericObservableArray class

--- DRObservableArray/Common/DRGenericObservableArray.m
+++ DRObservableArray/Common/DRGenericObservableArray.m
@@ -45,47 +45,47 @@
 
 - (void)setObjects:(NSArray *)objects
 {
-    [self willChangeObjects];
+    [self.observers willChangeObjects];
     _objects = [objects mutableCopy];
-    [self didSetObjects];
-    [self didChangeObjects];
+    [self.observers didSetObjects];
+    [self.observers didChangeObjects];
 }
 
 - (void)insertObject:(NSObject *)object atIndex:(NSUInteger)index
 {
-    [self willChangeObjects];
+    [self.observers willChangeObjects];
     [_objects insertObject:object atIndex:index];
-    [self didInsertObject:object atIndex:index];
-    [self didChangeObjects];
+    [self.observers didInsertObject:object atIndex:index];
+    [self.observers didChangeObjects];
 }
 
 - (void)removeObjectAtIndex:(NSUInteger)index
 {
-    [self willChangeObjects];
+    [self.observers willChangeObjects];
     id object = _objects[index];
     [_objects removeObjectAtIndex:index];
-    [self didRemoveObject:object atIndex:index];
-    [self didChangeObjects];
+    [self.observers didRemoveObject:object atIndex:index];
+    [self.observers didChangeObjects];
 }
 
 - (void)replaceObjectAtIndex:(NSUInteger)index withObject:(id)object
 {
-    [self willChangeObjects];
+    [self.observers willChangeObjects];
     id replacedObject = _objects[index];
     _objects[index] = object;
-    [self didReplaceObject:replacedObject atIndex:index withObject:object];
-    [self didChangeObjects];
+    [self.observers didReplaceObject:replacedObject atIndex:index withObject:object];
+    [self.observers didChangeObjects];
 }
 
 - (void)moveObjectAtIndex:(NSUInteger)index1 toIndex:(NSUInteger)index2
 {
     if (index2 == index1) return;
-    [self willChangeObjects];
+    [self.observers willChangeObjects];
     id object = _objects[index1];
     [_objects removeObjectAtIndex:index1];
     [_objects insertObject:object atIndex:index2];
-    [self didMoveObject:object fromIndex:index1 toIndex:index2];
-    [self didChangeObjects];
+    [self.observers didMoveObject:object fromIndex:index1 toIndex:index2];
+    [self.observers didChangeObjects];
 }
 
 - (void)exchangeObjectAtIndex:(NSUInteger)index1 withObjectAtIndex:(NSUInteger)index2
@@ -94,43 +94,6 @@
     [self moveObjectAtIndex:index1 toIndex:index2];
     [self moveObjectAtIndex:index1 < index2 ? index2 - 1 : index2 + 1
                     toIndex:index1];
-}
-
-#pragma mark - Events
-
-- (void)willChangeObjects
-{
-    [self.observers willChangeObjects];
-}
-
-- (void)didChangeObjects
-{
-    [self.observers didChangeObjects];
-}
-
-- (void)didSetObjects
-{
-    [self.observers didSetObjects];
-}
-
-- (void)didInsertObject:(id)object atIndex:(NSUInteger)index
-{
-    [self.observers didInsertObject:object atIndex:index];
-}
-
-- (void)didRemoveObject:(id)object atIndex:(NSUInteger)index
-{
-    [self.observers didRemoveObject:object atIndex:index];
-}
-
-- (void)didReplaceObject:(id)replacedObject atIndex:(NSUInteger)index withObject:(id)newObject
-{
-    [self.observers didReplaceObject:replacedObject atIndex:index withObject:newObject];
-}
-
-- (void)didMoveObject:(id)object fromIndex:(NSUInteger)fromIndex toIndex:(NSUInteger)toIndex
-{
-    [self.observers didMoveObject:object fromIndex:fromIndex toIndex:toIndex];
 }
 
 @end
