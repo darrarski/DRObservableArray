8c3f72d6b9d6ffa1fb286fbc41a7b781d72a100d
Dariusz Rybicki <darrarski@gmail.com>
2016-08-04 15:45:40 +0200

    Add example with UITableView and array shuffling

--- DRObservableArrayExample/UI/ViewController.m
+++ DRObservableArrayExample/UI/ViewController.m
@@ -4,21 +4,101 @@
 //
 
 #import "ViewController.h"
+#import "DRGenericObservableArray.h"
+#import "DRObservableArrayTableViewUpdater.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) NSObject <DRObservableArray, DRObservableMutableArray> *words;
+@property (nonatomic, strong) DRObservableArrayTableViewUpdater *wordsTableViewUpdater;
+
+@end
 
 @implementation ViewController
 
 - (instancetype)init
 {
-    if (self = [super initWithNibName:nil bundle:nil]) {
+    if (self = [super initWithStyle:UITableViewStylePlain]) {
         self.title = @"DRObservableArrayExample";
     }
     return self;
 }
 
-- (void)loadView
+- (void)viewDidLoad
 {
-    self.view = [[UIView alloc] initWithFrame:CGRectZero];
-    self.view.backgroundColor = [UIColor whiteColor];
+    [super viewDidLoad];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
+                                                                                           target:self
+                                                                                           action:@selector(shuffleButtonAction)];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    [self.words.observers addObserver:self.wordsTableViewUpdater];
+}
+
+#pragma mark - UI Actions
+
+- (void)shuffleButtonAction
+{
+    [self shuffleWords];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.words.objectsCount;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    NSString *word = [self.words objectAtIndex:(NSUInteger) indexPath.row];
+    cell.textLabel.text = word;
+
+    return cell;
+}
+
+#pragma mark - Words
+
+- (NSObject <DRObservableArray, DRObservableMutableArray> *)words
+{
+    if (!_words) {
+        DRGenericObservableArray *words = [[DRGenericObservableArray alloc] init];
+        words.objects = @[
+            @"jat", @"wise", @"genit", @"file", @"straw", @"cow", @"sleuth",
+            @"lunes", @"scan", @"gyn", @"luce", @"weft", @"bim", @"moit",
+            @"wrench", @"kempt", @"klepht", @"whiz", @"prawn", @"crud"
+        ];
+        _words = words;
+    }
+    return _words;
+}
+
+- (DRObservableArrayTableViewUpdater *)wordsTableViewUpdater
+{
+    if (!_wordsTableViewUpdater) {
+        __weak typeof(self) welf = self;
+        ObservableArrayTableViewUpdaterTableViewBlock tableViewBlock = ^UITableView * {
+            return welf.tableView;
+        };
+        ObservableArrayTableViewUpdaterSectionBlock sectionBlock = ^NSUInteger {
+            return 0;
+        };
+        _wordsTableViewUpdater = [[DRObservableArrayTableViewUpdater alloc] initWithTableViewBlock:tableViewBlock
+                                                                                      sectionBlock:sectionBlock];
+    }
+    return _wordsTableViewUpdater;
+}
+
+- (void)shuffleWords
+{
+    NSUInteger count = self.words.objectsCount;
+    if (count < 1) return;
+    for (NSUInteger index = 0; index < count - 1; ++index) {
+        NSUInteger remainingCount = count - index;
+        NSUInteger exchangeIndex = index + arc4random_uniform((u_int32_t )remainingCount);
+        [self.words exchangeObjectAtIndex:index withObjectAtIndex:exchangeIndex];
+    }
 }
 
 @end
