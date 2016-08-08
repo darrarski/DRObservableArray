57f559e6c4894a0839fa48d6a5d152e4a773ed87
Dariusz Rybicki <darrarski@gmail.com>
2016-08-08 21:19:33 +0200

    Make Examples List the first screen of the app

--- DRObservableArrayExample-iOS/Application/AppDelegate.m
+++ DRObservableArrayExample-iOS/Application/AppDelegate.m
@@ -4,14 +4,14 @@
 //
 
 #import "AppDelegate.h"
-#import "TableViewUpdaterExampleViewController.h"
+#import "ExamplesListViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[TableViewUpdaterExampleViewController alloc] init]];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ExamplesListViewController alloc] init]];
     [self.window makeKeyAndVisible];
     return YES;
 }
