b221d05f94503e83fbaccabe5fcab3eec278e39c
Dariusz Rybicki <darrarski@gmail.com>
2016-08-08 21:10:15 +0200

    Add Examples List view controller

---
+++ DRObservableArrayExample-iOS/UI/ExamplesList/ExamplesListViewController.h
@@ -0,0 +1,12 @@
+//
+// Created by Dariusz Rybicki on 08/08/16.
+// Copyright (c) 2016 Darrarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExamplesListViewController : UITableViewController
+
+- (instancetype)init;
+
+@end
---
+++ DRObservableArrayExample-iOS/UI/ExamplesList/ExamplesListViewController.m
@@ -0,0 +1,18 @@
+//
+// Created by Dariusz Rybicki on 08/08/16.
+// Copyright (c) 2016 Darrarski. All rights reserved.
+//
+
+#import "ExamplesListViewController.h"
+
+@implementation ExamplesListViewController
+
+- (instancetype)init
+{
+    if (self = [super initWithStyle:UITableViewStylePlain]) {
+        self.title = @"DRObservableArray iOS Examples";
+    }
+    return self;
+}
+
+@end
