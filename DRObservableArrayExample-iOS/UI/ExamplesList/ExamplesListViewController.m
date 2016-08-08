e7d62db640bc94a8cf7e970f377b6a75bef346a7
Dariusz Rybicki <darrarski@gmail.com>
2016-08-08 21:19:01 +0200

    Add Table View Updater Example to list of examples

--- DRObservableArrayExample-iOS/UI/ExamplesList/ExamplesListViewController.m
+++ DRObservableArrayExample-iOS/UI/ExamplesList/ExamplesListViewController.m
@@ -4,6 +4,7 @@
 //
 
 #import "ExamplesListViewController.h"
+#import "TableViewUpdaterExampleViewController.h"
 
 @implementation ExamplesListViewController
 
@@ -13,6 +14,48 @@
         self.title = @"DRObservableArray iOS Examples";
     }
     return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.textLabel.text = ^NSString *{
+        switch (indexPath.row) {
+            case 0:
+                return @"Table View Updater Example";
+            default:
+                return nil;
+        }
+    }();
+
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    switch (indexPath.row) {
+        case 0:
+            [self.navigationController pushViewController:[[TableViewUpdaterExampleViewController alloc] init]
+                                                 animated:YES];
+            break;
+        default:
+            break;
+    }
 }
 
 @end
