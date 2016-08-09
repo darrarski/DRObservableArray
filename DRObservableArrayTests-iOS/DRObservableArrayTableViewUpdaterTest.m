f619bffebc8edbf57d1738e7d1dbe7b3c659fead
Dariusz Rybicki <darrarski@gmail.com>
2016-08-10 00:20:12 +0200

    Update Table View Updater Test to use sut operation queue

--- DRObservableArrayTests-iOS/DRObservableArrayTableViewUpdaterTest.m
+++ DRObservableArrayTests-iOS/DRObservableArrayTableViewUpdaterTest.m
@@ -57,83 +57,117 @@
 
 - (void)testShouldInsertRowWhenInsertingObject
 {
-    [self.collection insertObject:@"G" atIndex:6];
-    NSArray *expectedOperationStrings = @[
-        [self.tableView stringForBeginUpdates],
-        [self.tableView stringForInsertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:6 inSection:self.sectionIndex]] withRowAnimation:UITableViewRowAnimationAutomatic],
-        [self.tableView stringForEndUpdates]
-    ];
-    dispatch_async(dispatch_get_main_queue(), ^{
+    [self performInBackgroundAndWait:^{
+        [self.collection insertObject:@"G" atIndex:6];
+    }];
+    [self performInSutQueueAndWait:^{
+        NSArray *expectedOperationStrings = @[
+            [self.tableView stringForBeginUpdates],
+            [self.tableView stringForInsertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:6 inSection:self.sectionIndex]] withRowAnimation:UITableViewRowAnimationAutomatic],
+            [self.tableView stringForEndUpdates]
+        ];
         XCTAssertEqualObjects(self.tableView.operationStrings, expectedOperationStrings, @"Should insert row when inserting object");
-    });
+    }];
 }
 
 - (void)testShouldDeleteRowWhenRemovingObject
 {
-    [self.collection removeObjectAtIndex:4];
-    NSArray *expectedOperationStrings = @[
-        [self.tableView stringForBeginUpdates],
-        [self.tableView stringForDeleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:4 inSection:self.sectionIndex]] withRowAnimation:UITableViewRowAnimationAutomatic],
-        [self.tableView stringForEndUpdates]
-    ];
-    dispatch_async(dispatch_get_main_queue(), ^{
+    [self performInBackgroundAndWait:^{
+        [self.collection removeObjectAtIndex:4];
+    }];
+    [self performInSutQueueAndWait:^{
+        NSArray *expectedOperationStrings = @[
+            [self.tableView stringForBeginUpdates],
+            [self.tableView stringForDeleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:4 inSection:self.sectionIndex]] withRowAnimation:UITableViewRowAnimationAutomatic],
+            [self.tableView stringForEndUpdates]
+        ];
         XCTAssertEqualObjects(self.tableView.operationStrings, expectedOperationStrings, @"Should delete row when removing object");
-    });
+    }];
 }
 
 - (void)testShouldReloadDataWhenSettingObjects
 {
-    [self.collection setObjects:@[@"1", @"2", @"3"]];
-    NSArray *expectedOperationStrings = @[
-        [self.tableView stringForBeginUpdates],
-        [self.tableView stringForReloadData],
-        [self.tableView stringForEndUpdates]
-    ];
-    dispatch_async(dispatch_get_main_queue(), ^{
+    [self performInBackgroundAndWait:^{
+        [self.collection setObjects:@[@"1", @"2", @"3"]];
+    }];
+    [self performInSutQueueAndWait:^{
+        NSArray *expectedOperationStrings = @[
+            [self.tableView stringForBeginUpdates],
+            [self.tableView stringForReloadData],
+            [self.tableView stringForEndUpdates]
+        ];
         XCTAssertEqualObjects(self.tableView.operationStrings, expectedOperationStrings, @"Should reload data when setting objects");
-    });
+    }];
 }
 
 - (void)testShouldReloadRowWhenReplacingObject
 {
-    [self.collection replaceObjectAtIndex:2 withObject:@"0"];
-    NSArray *expectedOperationStrings = @[
-        [self.tableView stringForBeginUpdates],
-        [self.tableView stringForReloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:2 inSection:self.sectionIndex]] withRowAnimation:UITableViewRowAnimationAutomatic],
-        [self.tableView stringForEndUpdates]
-    ];
-    dispatch_async(dispatch_get_main_queue(), ^{
+    [self performInBackgroundAndWait:^{
+        [self.collection replaceObjectAtIndex:2 withObject:@"0"];
+    }];
+    [self performInSutQueueAndWait:^{
+        NSArray *expectedOperationStrings = @[
+            [self.tableView stringForBeginUpdates],
+            [self.tableView stringForReloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:2 inSection:self.sectionIndex]] withRowAnimation:UITableViewRowAnimationAutomatic],
+            [self.tableView stringForEndUpdates]
+        ];
         XCTAssertEqualObjects(self.tableView.operationStrings, expectedOperationStrings, @"Should reload row when replacing object");
-    });
+    }];
 }
 
 - (void)testShouldMoveRowWhenMovingObject
 {
-    [self.collection moveObjectAtIndex:3 toIndex:1];
-    NSArray *expectedOperationStrings = @[
-        [self.tableView stringForBeginUpdates],
-        [self.tableView stringForMoveRowAtIndexPath:[NSIndexPath indexPathForRow:3 inSection:self.sectionIndex] toIndexPath:[NSIndexPath indexPathForRow:1 inSection:self.sectionIndex]],
-        [self.tableView stringForEndUpdates]
-    ];
-    dispatch_async(dispatch_get_main_queue(), ^{
+    [self performInBackgroundAndWait:^{
+        [self.collection moveObjectAtIndex:3 toIndex:1];
+    }];
+    [self performInSutQueueAndWait:^{
+        NSArray *expectedOperationStrings = @[
+            [self.tableView stringForBeginUpdates],
+            [self.tableView stringForMoveRowAtIndexPath:[NSIndexPath indexPathForRow:3 inSection:self.sectionIndex] toIndexPath:[NSIndexPath indexPathForRow:1 inSection:self.sectionIndex]],
+            [self.tableView stringForEndUpdates]
+        ];
         XCTAssertEqualObjects(self.tableView.operationStrings, expectedOperationStrings, @"Should move row when moving object");
-    });
+    }];
 }
 
 - (void)testShouldMoveTwoRowsWhenExchagingObjects
 {
-    [self.collection exchangeObjectAtIndex:2 withObjectAtIndex:5];
-    NSArray *expectedOperationStrings = @[
-        [self.tableView stringForBeginUpdates],
-        [self.tableView stringForMoveRowAtIndexPath:[NSIndexPath indexPathForRow:2 inSection:self.sectionIndex] toIndexPath:[NSIndexPath indexPathForRow:5 inSection:self.sectionIndex]],
-        [self.tableView stringForEndUpdates],
-        [self.tableView stringForBeginUpdates],
-        [self.tableView stringForMoveRowAtIndexPath:[NSIndexPath indexPathForRow:4 inSection:self.sectionIndex] toIndexPath:[NSIndexPath indexPathForRow:2 inSection:self.sectionIndex]],
-        [self.tableView stringForEndUpdates]
-    ];
-    dispatch_async(dispatch_get_main_queue(), ^{
+    [self performInBackgroundAndWait:^{
+        [self.collection exchangeObjectAtIndex:2 withObjectAtIndex:5];
+    }];
+    [self performInSutQueueAndWait:^{
+        NSArray *expectedOperationStrings = @[
+            [self.tableView stringForBeginUpdates],
+            [self.tableView stringForMoveRowAtIndexPath:[NSIndexPath indexPathForRow:2 inSection:self.sectionIndex] toIndexPath:[NSIndexPath indexPathForRow:5 inSection:self.sectionIndex]],
+            [self.tableView stringForEndUpdates],
+            [self.tableView stringForBeginUpdates],
+            [self.tableView stringForMoveRowAtIndexPath:[NSIndexPath indexPathForRow:4 inSection:self.sectionIndex] toIndexPath:[NSIndexPath indexPathForRow:2 inSection:self.sectionIndex]],
+            [self.tableView stringForEndUpdates]
+        ];
         XCTAssertEqualObjects(self.tableView.operationStrings, expectedOperationStrings, @"Should move two rows when exchanging objects");
+    }];
+}
+
+#pragma mark - Helpers
+
+- (void)performInBackgroundAndWait:(void (^)())block
+{
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
+        block();
+        dispatch_semaphore_signal(semaphore);
     });
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+}
+
+- (void)performInSutQueueAndWait:(void (^)())block
+{
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    [self.sut.operationQueue addOperationWithBlock:^{
+        block();
+        dispatch_semaphore_signal(semaphore);
+    }];
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
 }
 
 @end
